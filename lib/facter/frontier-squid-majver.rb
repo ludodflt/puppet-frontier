3bc1ca440fc5b65ebc1437be04230c7be572e9f7
Ludovic DUFLOT <ludovic.duflot@lapp.in2p3.fr>
2021-11-05 15:42:12 +0100

    check os.family

--- lib/facter/frontier-squid-majver.rb
+++ lib/facter/frontier-squid-majver.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/ruby
 require 'facter'
+if Facter.value(:os)['family'] == 'RedHat'
+    version = Facter::Util::Resolution.exec("rpm -q frontier-squid --queryformat '[%{NAME} %{VERSION}-%{RELEASE}\n]'")
 
-version = Facter::Util::Resolution.exec("rpm -q frontier-squid --queryformat '[%{NAME} %{VERSION}-%{RELEASE}\n]'")
-
-Facter.add("#{version.split[0]}_majver".gsub('-','_')) do
-    setcode do
-          "#{version.split[1].split('.')[0]}"
+    Facter.add("#{version.split[0]}_majver".gsub('-','_')) do
+        setcode do
+            "#{version.split[1].split('.')[0]}"
+        end
     end
 end
